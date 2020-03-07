7f2c0780ab99ac63f0672e52eb090bfeda5c5839
Juan Young Huh <juan.huh@evry.com>
2020-03-07 18:09:41 +0100

    x

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class MainScreen extends StatefulWidget {
   @override
@@ -6,14 +7,31 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  CalendarController _calendarController;
+
+  @override
+  void initState() {
+    super.initState();
+    _calendarController = CalendarController();
+  }
+
+  @override
+  void dispose() {
+    _calendarController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {
-
-        },
+        onPressed: () {},
+      ),
+      body: Container(
+        child: TableCalendar(
+          calendarController: _calendarController,
+        ),
       ),
     );
   }
