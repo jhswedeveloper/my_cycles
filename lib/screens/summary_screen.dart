506084590d7ec1bc247f80803ce39093d2e498f5
Juan Young Huh <juan.huh@evry.com>
2020-03-08 12:29:01 +0100

    x

--- lib/screens/summary_screen.dart
+++ lib/screens/summary_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:mycycles/screens/profile_screen.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 class SummaryScreen extends StatefulWidget {
   @override
@@ -26,24 +29,83 @@ class _SummaryScreenState extends State<SummaryScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            Card(
+      body: StaggeredGridView.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 12.0,
+        mainAxisSpacing: 12.0,
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        children: <Widget>[
+          _buildTile(
+            Padding(
+              padding: const EdgeInsets.all(24.0),
               child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[Text('Cycle Length'), Text('28 Days')],
-              ),
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Expanded(
+                      child: CircularPercentIndicator(
+                        radius: 120.0,
+                        lineWidth: 13.0,
+                        animation: true,
+                        percent: 0.7,
+                        center: new Text(
+                          "20 DAYS",
+                          style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+                        ),
+                        header: Padding(
+                          padding: const EdgeInsets.only(bottom: 8.0),
+                          child: new Text(
+                            "Cycle length",
+                            style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 17.0),
+                          ),
+                        ),
+                        circularStrokeCap: CircularStrokeCap.round,
+                        progressColor: Colors.pink,
+                      ),
+                    ),
+                  ]),
             ),
-            Card(
+            onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (_) => ProfileScreen())),
+          ),
+          _buildTile(
+            Padding(
+              padding: const EdgeInsets.all(24.0),
               child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[Text('Period Length'), Text('3 Days')],
-              ),
-            )
-          ],
-        ),
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Expanded(
+                      child: CircularPercentIndicator(
+                        radius: 120.0,
+                        lineWidth: 13.0,
+                        animation: true,
+                        percent: 0.7,
+                        center: new Text(
+                          "20 DAYS",
+                          style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+                        ),
+                        header: Padding(
+                          padding: const EdgeInsets.only(bottom: 8.0),
+                          child: new Text(
+                            "Cycle length",
+                            style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 17.0),
+                          ),
+                        ),
+                        circularStrokeCap: CircularStrokeCap.round,
+                        progressColor: Colors.pink,
+                      ),
+                    ),
+                  ]),
+            ),
+            onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (_) => ProfileScreen())),
+          )
+        ],
+        staggeredTiles: [
+          StaggeredTile.extent(1, 220.0),
+          StaggeredTile.extent(1, 220.0),
+          StaggeredTile.extent(2, 220.0),
+          StaggeredTile.extent(2, 110.0),
+        ],
       ),
     );
   }
