3c28169dc1e6fedba2238a30da146ce901eb3900
Juan Young Huh <juan.huh@evry.com>
2020-03-08 11:46:15 +0100

    x

--- lib/screens/summary_screen.dart
+++ lib/screens/summary_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SummaryScreen extends StatefulWidget {
@@ -6,10 +7,44 @@ class SummaryScreen extends StatefulWidget {
 }
 
 class _SummaryScreenState extends State<SummaryScreen> {
+  Widget _buildTile(Widget child, {Function() onTap}) {
+    return Material(
+      elevation: 14.0,
+      borderRadius: BorderRadius.circular(12.0),
+      shadowColor: Color(0x802196F3),
+      child: InkWell(
+          // Do onTap() if it isn't null, otherwise do print()
+          onTap: onTap != null
+              ? () => onTap()
+              : () {
+                  print('Not set yet');
+                },
+          child: child),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('hello'),
+    return Scaffold(
+      body: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[Text('Cycle Length'), Text('28 Days')],
+              ),
+            ),
+            Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[Text('Period Length'), Text('3 Days')],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
