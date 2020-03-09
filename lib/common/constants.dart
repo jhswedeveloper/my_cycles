f8f6f9f46ee9da996c7c92306e3ba5399b328dba
Juan Young Huh <juan.huh@evry.com>
2020-03-09 16:31:08 +0100

    added login screen

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,3 +1,26 @@
 import 'package:flutter/material.dart';
 
 const kMainColor = Color(0xff465466);
+
+final kHintTextStyle = TextStyle(
+  color: Colors.white54,
+  fontFamily: 'OpenSans',
+);
+
+final kLabelStyle = TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+  fontFamily: 'OpenSans',
+);
+
+final kBoxDecorationStyle = BoxDecoration(
+  color: Color(0xFF6CA8F1),
+  borderRadius: BorderRadius.circular(10.0),
+  boxShadow: [
+    BoxShadow(
+      color: Colors.black12,
+      blurRadius: 6.0,
+      offset: Offset(0, 2),
+    ),
+  ],
+);
