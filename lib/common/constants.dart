86d0431314535a0ead8e235236ba503c7788a735
Juan Young Huh <juan.huh@evry.com>
2020-03-11 00:34:35 +0100

    x

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -14,7 +14,7 @@ final kLabelStyle = TextStyle(
 );
 
 final kBoxDecorationStyle = BoxDecoration(
-  color: Color(0xFF6CA8F1),
+  color: Color(0xFF8542e3),
   borderRadius: BorderRadius.circular(10.0),
   boxShadow: [
     BoxShadow(
